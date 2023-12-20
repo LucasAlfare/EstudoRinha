4e3263e88e5a79ce04d03c1291dc54137ffbb181
LucasAlfare <god-lucas@hotmail.com>
2023-12-20 18:50:45 -0300

    adiciona comentários de documentação automáticos (GPT 3.5)

--- src/main/kotlin/ArrayColumnImpl.kt
+++ src/main/kotlin/ArrayColumnImpl.kt
@@ -9,13 +9,35 @@ import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.statements.jdbc.JdbcConnectionImpl
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
+/**
+ * Extension function for a `Table` to define an array column.
+ *
+ * @param name The name of the array column.
+ * @param columnType The column type of the array elements.
+ * @return The defined array column.
+ */
 fun <T> Table.array(name: String, columnType: ColumnType): Column<Array<T>> =
   registerColumn(name, ArrayColumnType(columnType))
 
+/**
+ * Custom `ColumnType` implementation to support array columns.
+ *
+ * @param type The column type of the array elements.
+ */
 class ArrayColumnType(private val type: ColumnType) : ColumnType() {
 
+  /**
+   * Checks if the database supports arrays.
+   *
+   * @return `true` if arrays are supported, `false` otherwise.
+   */
   private fun supportsArrays() = true
 
+  /**
+   * Gets the SQL type representation for the array column.
+   *
+   * @return The SQL type representation.
+   */
   override fun sqlType(): String = buildString {
     if (!supportsArrays()) {
       append("TEXT")
@@ -25,6 +47,12 @@ class ArrayColumnType(private val type: ColumnType) : ColumnType() {
     }
   }
 
+  /**
+   * Converts the value to a format suitable for database storage.
+   *
+   * @param value The value to be converted.
+   * @return The converted value.
+   */
   override fun valueToDB(value: Any?): Any? {
     if (!supportsArrays())
       return "'NOT SUPPORTED'"
@@ -38,6 +66,12 @@ class ArrayColumnType(private val type: ColumnType) : ColumnType() {
     }
   }
 
+  /**
+   * Converts the value retrieved from the database to the corresponding Kotlin type.
+   *
+   * @param value The value retrieved from the database.
+   * @return The converted Kotlin type value.
+   */
   override fun valueFromDB(value: Any): Any {
     if (!supportsArrays()) {
       val clazz = type::class
@@ -55,9 +89,15 @@ class ArrayColumnType(private val type: ColumnType) : ColumnType() {
     if (value is Array<*>) {
       return value
     }
-    error("Array does not support for this database")
+    error("Array is not supported for this database")
   }
 
+  /**
+   * Converts the non-null value to a format suitable for database storage.
+   *
+   * @param value The non-null value to be converted.
+   * @return The converted value.
+   */
   override fun notNullValueToDB(value: Any): Any {
     if (!supportsArrays())
       return "'NOT SUPPORTED'"
@@ -68,7 +108,7 @@ class ArrayColumnType(private val type: ColumnType) : ColumnType() {
 
       val columnType = type.sqlType().split("(")[0]
       val jdbcConnection = (TransactionManager.current().connection as JdbcConnectionImpl).connection
-      return jdbcConnection.createArrayOf(columnType, value) ?: error("Can't create non null array for $value")
+      return jdbcConnection.createArrayOf(columnType, value) ?: error("Can't create non-null array for $value")
     } else {
       return super.notNullValueToDB(value)
     }
